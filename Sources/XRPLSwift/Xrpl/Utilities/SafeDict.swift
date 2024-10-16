d1f197574d63f34a8e10f2792c95edc25228ffe3
Minji Kim <mimin@girin.app>
2024-10-16 14:47:04 +0900

    fix: dispatchqueue concurrency

--- Sources/XRPLSwift/Xrpl/Utilities/SafeDict.swift
+++ Sources/XRPLSwift/Xrpl/Utilities/SafeDict.swift
@@ -22,24 +22,24 @@ class SafeDict<Key: Hashable, Value> {
 
     func get(key: Key) -> Value? {
         var result: Value?
-        dispatchQueue.sync {
+        dispatchQueue.sync { [unowned self] in
             result = self.threadUnsafeDict[key]
         }
         return result
     }
     
     func contains(key: Key) -> Bool {
-        var result: Bool?
-        dispatchQueue.sync {
+        var result: Bool = false
+        dispatchQueue.sync { [unowned self] in
             result = self.threadUnsafeDict.contains(where: { $0.key == key })
         }
         
-        return result!
+        return result
     }
     
     func getByIndex(_ key: Dictionary<Key, Value>.Index) -> Value? {
         var result: Value?
-        dispatchQueue.sync {
+        dispatchQueue.sync { [unowned self] in
             result = self.threadUnsafeDict[key].value
         }
         
@@ -48,7 +48,7 @@ class SafeDict<Key: Hashable, Value> {
     
     func firstIndex(_ searchKey: Key) -> Dictionary<Key, Value>.Index? {
         var idx: Dictionary<Key, Value>.Index?
-        dispatchQueue.sync {
+        dispatchQueue.sync { [unowned self] in
             idx = self.threadUnsafeDict.firstIndex(where: {$0.key == searchKey})
         }
         
@@ -57,7 +57,7 @@ class SafeDict<Key: Hashable, Value> {
     
     func keys() -> Dictionary<Key, Value>.Keys {
         var keys: Dictionary<Key, Value>.Keys!
-        dispatchQueue.sync {
+        dispatchQueue.sync { [unowned self] in
             keys = self.threadUnsafeDict.keys
         }
         
@@ -65,9 +65,8 @@ class SafeDict<Key: Hashable, Value> {
     }
 
     func set(key: Key, value: Value?) {
-        dispatchQueue.sync {
-            self.threadUnsafeDict.removeValue(forKey: key)
-
+        self.remove(key: key)
+        dispatchQueue.async(flags: .barrier) { [unowned self] in
             // added for preventing crash against below
             if let value {
                 self.threadUnsafeDict.updateValue(value, forKey: key)
@@ -76,8 +75,8 @@ class SafeDict<Key: Hashable, Value> {
     }
     
     func remove(key: Key) {
-        dispatchQueue.sync {
-            guard let index = self.firstIndex(key) else { return }
+        guard let index = self.firstIndex(key) else { return }
+        dispatchQueue.async(flags: .barrier) { [unowned self] in
             self.threadUnsafeDict.remove(at: index)
         }
     }
